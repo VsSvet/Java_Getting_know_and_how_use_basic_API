d7efa19b5e26a0ae61134cba6340655994a0139c
Vostrova Svetlana <masyanyamasyanya1@gmail.com>
2023-04-19 16:31:28 +0300

    add task1

--- Lesson1/src/Task1.java
+++ Lesson1/src/Task1.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Task1 {
+public class Task1 {
+    public static void main(String[] args){
+        for(int i = 1; i < 1001; i++) {
+            System.out.println(i);
+        }
+    }
 }
