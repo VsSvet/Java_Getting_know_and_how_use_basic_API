07a709d3cbd586196765351d7d767a2d66ad8651
Vostrova Svetlana <masyanyamasyanya1@gmail.com>
2023-04-20 23:32:50 +0300

    add task 4, 5

--- Lesson1/Task4.java
+++ Lesson1/Task4.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество элементов: ");
         int num = sc.nextInt();
         int count = 0;
         ArrayList<Integer> result = new ArrayList<>();
         int[] numbers = new int[num];
         for (int i = 0; i < num; i++) {
+            System.out.println("Введите новый элемент: ");
             numbers[i] = sc.nextInt();
             if (i % 2 == 0) {
                 result.add(numbers[i]);
--- Lesson1/Task5.java
+++ Lesson1/Task5.java
@@ -1,2 +1,23 @@
+import java.util.*;
+
 public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        System.out.println("Введите список чисел через пробел: ");
+        String number = sc.nextLine();
+        List<String> numbers = List.of(number.split(" "));
+        Set<String> set = new HashSet<>(List.of(number.split(" ")));
+        for (String name : set) {
+            for (String s : numbers) {
+                if (name.equals(s)) {
+                    count += 1;
+                }
+            }
+            if (count == 1){
+                System.out.println(name);
+            }
+            count = 0;
+        }
+    }
 }
