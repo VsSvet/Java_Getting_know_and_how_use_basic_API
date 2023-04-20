7d99184cfea3e2d87f360c34d9e4ea20629cfdd9
Vostrova Svetlana <masyanyamasyanya1@gmail.com>
2023-04-20 21:38:16 +0300

    add task 4

--- Lesson1/Task4.java
+++ Lesson1/Task4.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+        int[] numbers = new int[num];
+        for (int i = 0; i < num; i++) {
+            numbers[i] = sc.nextInt();
+            if (i % 2 == 0) {
+                result.add(numbers[i]);
+                count += numbers[i];
+            }
+        }
+        System.out.println(count);
+        System.out.println(result);
+    }
 }
